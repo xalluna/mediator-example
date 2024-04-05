cf81848854fd5188b699e71c11535abb7feeec60
Deanna <deannacshort@gmail.com>
2024-04-05 00:39:05 -0500

    wip: basic mediator example with air traffic control

---
+++ MediatorTwo/MediatorTwo/Common/IControlTower.cs
@@ -0,0 +1,22 @@
+﻿using MediatorTwo.Features;
+
+namespace MediatorTwo.Common;
+
+public interface IControlTower
+{
+    void RequestTakeoff(Airplane airplane);
+    void RequestLanding(Airplane airplane);
+}
+
+public class AirTrafficControlTower : IControlTower
+{
+    public void RequestTakeoff(Airplane airplane) 
+    {
+        airplane.NotifyAirTrafficControl("Requesting takeoff clearance.");
+    }
+ 
+    public void RequestLanding(Airplane airplane) 
+    {
+        airplane.NotifyAirTrafficControl("Requesting landing clearance.");
+    }
+}
--- MediatorTwo/MediatorTwo/Program.cs
+++ MediatorTwo/MediatorTwo/Program.cs
@@ -6,3 +6,19 @@ mediator.RegisterHandler(typeof(PrintRequest), new PrintRequestHandler());
 var outMessage = mediator.Handle(new PrintRequest("we mediate"));
 Console.WriteLine(outMessage);
 
+
+//Air Control Example
+var controlTower = new AirTrafficControlTower();
+ 
+var airplane1 = new Airplane(controlTower)
+{
+    Name = "Barbie"
+};
+
+var airplane2 = new Airplane(controlTower)
+{
+    Name = "Ken"
+};
+
+airplane1.RequestTakeoff();
+airplane2.RequestLanding();
